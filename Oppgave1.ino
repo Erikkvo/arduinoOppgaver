246223132868d78e2e6eaa8c25129770acdd03cb
haavarop <haavard@vaage.com>
2016-04-13 20:15:04 +0200

    Added files via upload

---
+++ Oppgave1.ino
@@ -0,0 +1,24 @@
+
+//Velkommen til Arduino IDE, det er i dette vinduet du kommer til å programmere din Arduino
+//for å se et forslag til hvordan koble opp kretsen, gå hit: https://123d.circuits.io/circuits/1951827-oppgave-1
+
+void setup() {
+  //void setup() forteller du Arudionen hvilke portet på kortet du skal bruke, og hva de skal brukes til.
+  //pinMode(port, tilstand)
+  
+  pinMode(13, OUTPUT); //Her forteller du kortet at du vil bruke port 13 på kortet som OUTPUT, altså at den skal sende ut strøm
+  
+  //Merk deg at alle linjer som starter med "//" fungerer som kommentarer, og blir ikke lest av datamaskinen. Disse linjene er for oss mennesker. 
+  //En annen ting du kan merke deg er at alle linjene som står mellom { } slutter på en ;
+
+}
+
+void loop() {
+  //I void loop() "skjer koden" din. Det er her du forteller at lys-dioden din skal lyse, eller noe annet du måtte ønske.
+  //Din oppgave er å få en LED-lampe til til blinke.
+  //for å gjøre dette må du bruke to funksjoner: digitalWrite(port, tilstand); og dealy(ms);
+  //med digitalWrite(port, tilstand); forteller forteller du til kortet at port skal ha tilstand HIGH eller LOW. 
+  //delay(ms); fungerer slik at koden venter i så mange millisekunder
+
+  //Prøv å få et lys på port 13 på arduinonen til å blinke med 250 millisekunder mellom HIGH og LOW. 
+}
---
+++ Oppgave2.ino
@@ -0,0 +1,29 @@
+
+//I denne oppgaven skal du bruke et potentimeter til å sende verdier tilbake til Arduionen, som du igjen skal bruke til å bestemme blinkehastigheten på et lys. 
+//Til denne oppgaven kommer til du til trenge et potentimeter. De ser slik ut: http://www.exploringarduino.com/wp-content/uploads/2013/06/10k-ohm-trimpot-400x247.jpg
+//Dersom du ikke har et slikt i ditt kit, rekk opp en hånd. 
+//Forslag til krets: https://123d.circuits.io/circuits/1952080-oppgave2 
+
+int sensorPin = 0;    // The potentiometer is connected to analog pin 0
+                      //Merk at det er en forskjell mellom analoge og digitale porter, porten det er snakk om er merket A0
+
+int ledPin = 13;      // The LED is connected to digital pin 13
+
+void setup(){
+  pinMode(ledPin, OUTPUT); //<-- Is the same as "pinMode(13, OUTPUT);
+                           //Vi trenger ikke å fortelle en analog port om en den skal være input eller output, så vi skriver ikke noe på den. 
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue; //Her oppretter vi bare en varibel som vi skal bruke senere 
+  
+  sensorValue = analogRead(sensorPin); //Her blir sensorValue satt til det samme som port A0 leser. 
+  
+  digitalWrite(ledPin, HIGH);     // Turn the LED on
+  
+  delay(sensorValue);             // Pause for sensorValue milliseconds 
+  
+  digitalWrite(ledPin, LOW);      // Turn the LED off
+  
+  delay(sensorValue);             // Pause for sensorValue milliseconds
+}
